967a1d95594c2895b19defc94fdef3c705382651
yunhozz <qkrdbsgh1121@naver.com>
2023-03-30 17:30:40 +0900

    feat: JWT 필터 추가

---
+++ auth-service/src/main/java/com/authservice/config/security/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package com.authservice.config.security.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error(accessDeniedException.getMessage());
+        response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+    }
+}
---
+++ auth-service/src/main/java/com/authservice/config/security/jwt/JwtFilter.java
@@ -0,0 +1,48 @@
+package com.authservice.config.security.jwt;
+
+import io.jsonwebtoken.lang.Strings;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("[REQUEST URL] " + request.getRequestURL());
+        String headerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        resolveToken(headerToken).ifPresent(token -> {
+            if (jwtProvider.isValidToken(token)) {
+                log.info("JWT Token is Authenticated!");
+                Authentication authentication = jwtProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        });
+
+        filterChain.doFilter(request, response);
+    }
+
+    private Optional<String> resolveToken(String token) {
+        return Strings.hasText(token) ? resolveParts(token) : Optional.empty();
+    }
+
+    private Optional<String> resolveParts(String token) {
+        String[] parts = token.split(" ");
+        return parts.length == 2 && parts[0].equals("Bearer") ? Optional.ofNullable(parts[1]) : Optional.empty();
+    }
+}
