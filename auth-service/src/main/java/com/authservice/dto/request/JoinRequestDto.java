8341d007131fba2aa032848ad23fdb46ca921f5c
yunhozz <qkrdbsgh1121@naver.com>
2023-04-01 22:01:59 +0900

    feat: 유저 관련 request DTO 추가

---
+++ auth-service/src/main/java/com/authservice/dto/request/JoinRequestDto.java
@@ -0,0 +1,29 @@
+package com.authservice.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JoinRequestDto {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String parentRegion;
+
+    @NotBlank
+    private String childRegion;
+}
