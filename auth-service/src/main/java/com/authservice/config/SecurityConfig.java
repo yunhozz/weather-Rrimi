20ed267c4795c169a0162427a9294b160d098fb9
yunhozz <qkrdbsgh1121@naver.com>
2023-04-01 22:02:29 +0900

    feat: 시큐리티 설정 정보 추가

---
+++ auth-service/src/main/java/com/authservice/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.authservice.config;
+
+import com.authservice.common.security.jwt.JwtAccessDeniedHandler;
+import com.authservice.common.security.jwt.JwtAuthenticationEntryPoint;
+import com.authservice.common.security.jwt.JwtFilter;
+import com.authservice.common.security.session.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtFilter jwtFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        // endpoint 별 권한 설정
+        security
+                .cors().and()
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll();
+
+        // 정책 설정
+        security
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // JWT
+
+        // security 세션, jwt 관련 설정
+        security
+                .userDetailsService(userDetailsService)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint);
+
+        // TODO: 2023/03/31 OAuth2 설정
+
+        return security.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
