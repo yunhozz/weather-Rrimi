ea7bf7d8d92411b99f5b665c2772c83003438df1
yunhozz <qkrdbsgh1121@naver.com>
2023-04-01 22:01:13 +0900

    feat: 쿼리 메소드 추가

--- auth-service/src/main/java/com/authservice/infra/UserRepository.java
+++ auth-service/src/main/java/com/authservice/infra/UserRepository.java
@@ -7,5 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUserId(String userId);
     Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
