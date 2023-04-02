0b54c474089b84dbdb0dd31c9a677a8870d66cf6
yunhozz <qkrdbsgh1121@naver.com>
2023-04-02 15:46:01 +0900

    rename: 빈 충돌로 인한 클래스명 변경

--- auth-service/src/main/java/com/authservice/common/security/session/UserDetailsCustomService.java
+++ auth-service/src/main/java/com/authservice/common/security/session/UserDetailsCustomService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsCustomService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
